9af2eae8b00c234e7ba9dc1e84094af7bc21e2bd
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2024-07-29 10:58:28 +0900

    User register method finished

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 // Import models and dependencies
+const user = require("../models/user");
 const models = require("../models/user");
 
 // Test actions
@@ -9,20 +10,50 @@ const userTest = (req, res) => {
 };
 
 // User register
-const register = (req, res) => {
-  // Collect data
-  let params = req.body;
+const register = async (req, res) => {
+  try {
+    // Collect data
+    let params = req.body;
 
-  if (!params.name || !params.nick || !params.password || !params.email) {
-    return res.status(400).json({
-      message: "Incomplete data",
+    // Validate required fields
+    if (!params.name || !params.nick || !params.password || !params.email) {
+      return res.status(400).json({
+        message: "Incomplete data",
+      });
+    }
+
+    // Create a new user
+    let user_to_save = new user(params);
+
+    // User duplicated check
+    const users = await user.find({
+      $or: [
+        { email: user_to_save.email.toLowerCase() },
+        { nick: user_to_save.nick.toLowerCase() },
+      ],
+    });
+
+    if (users.length >= 1) {
+      return res.status(409).json({
+        status: "success",
+        message: "User already exists",
+      });
+    }
+
+    // Save the new user to the database
+    const savedUser = await user_to_save.save();
+
+    return res.status(200).json({
+      status: "success",
+      message: "User registered successfully",
+      user: savedUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error in the server",
+      error: error.message,
     });
   }
-
-  return res.status(200).json({
-    message: "User registered successfully",
-    params,
-  });
 };
 
 // Export actions
