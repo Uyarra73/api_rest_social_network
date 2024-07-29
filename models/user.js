9ea42c3f3e515e6573e3e1d9172209a8d4b61f1b
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2024-07-29 09:18:58 +0900

    User schema defined

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const {Schema, model} = require("mongoose");
+
+// User schema
+const UserSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: String,
+    nick: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: "user_role"
+    },
+    image: {
+        type: String,
+        default: "default_image.jpg"
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = model("User", UserSchema, "users");
